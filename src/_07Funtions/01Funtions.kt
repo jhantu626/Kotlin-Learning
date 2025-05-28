5d77fd8bb6132dea802d0388b00ecbb878e6945a
jhantu626 <jhantubala626@gmail.com>
2025-05-28 17:23:14 +0530

    a

--- src/_07Funtions/01Funtions.kt
+++ src/_07Funtions/01Funtions.kt
@@ -1,6 +1,7 @@
 fun main(){
     println("THe sum of two number is ${sum(10,20)}")
     println(square(10))
+    println(sayHello())
 }
 
 fun sum(a:Int,b:Int):Int{
@@ -10,3 +11,14 @@ fun sum(a:Int,b:Int):Int{
 fun square(a:Int):Long{
     return (a*a).toLong();
 }
+
+// Parameter with default value
+fun sayHello(name:String="Hello, User"):String{
+    return name;
+}
+
+
+
+
+
+
